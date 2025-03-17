c01d8e9f499c694b0a96df2a3f8978756cfee2b4
Hananjaluta <123709562+Hananjaluta@users.noreply.github.com>
2025-03-17 17:35:37 -0400

    Update HelloWorld.java

--- src/Day01/HelloWorld.java
+++ src/Day01/HelloWorld.java
@@ -3,5 +3,8 @@ package Day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("my first line i wrote from intelliji witch is my local repasitory ");
+
+        System.out.println("my first line in remot not local  ");
+          System.out.println("lets see what will happen ");
     }
 }
