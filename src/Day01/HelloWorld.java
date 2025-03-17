52fa87f51561c648a8cae26be1dd88fd55c191cd
Hananjaluta <123709562+Hananjaluta@users.noreply.github.com>
2025-03-17 17:44:07 -0400

    Update HelloWorld.java
    fetch

--- src/Day01/HelloWorld.java
+++ src/Day01/HelloWorld.java
@@ -3,7 +3,7 @@ package Day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("my first line i wrote from intelliji witch is my local repasitory ");
-
+         System.out.println("lets try fetch from remot to local   ");
         System.out.println("my first line in remot not local  ");
           System.out.println("lets see what will happen ");
     }
