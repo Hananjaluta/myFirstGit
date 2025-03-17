c7965d8935579e60b3d007d2ed42d7a43c645447
Hanan Jaluta <hanan.k.jaluta@gmail.com>
2025-03-17 17:34:57 -0400

    from local

--- src/Day01/HelloWorld.java
+++ src/Day01/HelloWorld.java
@@ -1,4 +1,7 @@
 package Day01;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("my first line i wrote from intelliji witch is my local repasitory ");
+    }
 }
